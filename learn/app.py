93b81225f4668f7713969f7987eea7c6239b869b
africanquant <masesidan@gmail.com>
2022-03-12 19:02:31 +0300

    changes on dash core and html module

--- learn/app.py
+++ learn/app.py
@@ -1,6 +1,6 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 import plotly.graph_objects as go
 import plotly.express as px
 from dash.dependencies import Input, Output
